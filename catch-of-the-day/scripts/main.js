2a14d19005ee8034b48b7712e9fd9bf335fdc5f9
bryan huizi <bryanhuizi@gmail.com>
2016-06-05 12:27:52 -0700

    adds | 09 event listeners

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -5,6 +5,7 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 var h = require('./helpers');
@@ -76,9 +77,18 @@ var Inventory = React.createClass({
   StorePicker
 */
 var StorePicker = React.createClass({
+  mixins: [History],
+  goToStore: function(event){
+    event.preventDefault();
+    // get the data from the input
+    var storeId = this.refs.storeId.value;
+    // transition from <StorePicker /> to <App />
+    this.history.pushState(null, '/store/' + storeId);
+  },
+
   render: function(){
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input type='text' ref='storeId' defaultValue={h.getFunName()} required/>
         <input type='Submit'/>
