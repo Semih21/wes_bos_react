02f8d8e86df652fb2916b57e8f83e07f11956375
bryan huizi <bryan.huizi@modop.com>
2016-06-07 09:19:12 -0700

    add | 14 displays order state w/ jsx

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -49,7 +49,7 @@ var App = React.createClass({
             {Object.keys(this.state.fishes).map(this.renderFish)}
           </ul>
         </div>
-          <Order />
+          <Order fishes={this.state.fishes} order={this.state.order}/>
           <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
@@ -81,6 +81,7 @@ var Fish = React.createClass({
     )
   }
 });
+
 /*
   Add Fish Form
   <AddFishForm />
@@ -147,9 +148,47 @@ var Header = React.createClass({
 */
 
 var Order = React.createClass({
+  renderOrder: function(key){
+      var fish = this.props.fishes[key];
+      var count = this.props.order[key];
+      if(!fish) {
+        return <li key={key}>Sorry, fish is no longer available</li>
+      }
+
+      return (
+        <li>
+          <span>{count}lbs</span>
+          <span>{fish.name}</span>
+          <span className='price'>{h.formatPrice(count * fish.price)}</span>
+        </li>
+      )
+
+  },
   render: function(){
+    var orderIds = Object.keys(this.props.order);
+    var total = orderIds.reduce((prevTotal, key) =>{
+        var fish = this.props.fishes[key];
+        var count = this.props.order[key];
+        var isAvailable = fish && fish.status === 'available';
+
+        if(fish && isAvailable){
+          return prevTotal + (count * parseInt(fish.price) || 0);
+        }
+
+        return prevTotal
+    }, 0);
+
     return(
-      <p>Order</p>
+      <div className='order-wrap'>
+        <h2 className='order-title'>Your Order</h2>
+        <ul className='order'>
+          {orderIds.map(this.renderOrder)}
+          <li className='total'>
+            <strong>Total:</strong>
+            {h.formatPrice(total)}
+          </li>
+        </ul>
+      </div>
     )
   }
 });
@@ -177,6 +216,7 @@ var Inventory = React.createClass({
   StorePicker
   <StorePicker />
 */
+
 var StorePicker = React.createClass({
   mixins: [History],
   goToStore: function(event){
