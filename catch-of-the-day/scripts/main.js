6d7fdd86a3a7a8893b3a4260d0403491d1d7f7a6
bryan huizi <bryan.huizi@modop.com>
2016-06-21 09:33:50 -0700

    adds | 22 es2015 modules > App

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -1,33 +1,21 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-var ReactRouter = require('react-router');
-var Router = ReactRouter.Router;
-var Route = ReactRouter.Route;
-var History = ReactRouter.History;
-var createBrowserHistory = require('history/lib/createBrowserHistory');
-
-var h = require('./helpers');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router';
+import { createHistory } from 'history';
 
 /*
   import componenents
 */
 
 import App from './components/App';
-import Header from './components/Header';
-import Inventory from './components/Inventory';
-import AddFishForm from './components/AddFishForm';
-import Order from './components/Order';
-import Fish from './components/Fish';
 import NotFound from './components/NotFound';
 import StorePicker from './components/StorePicker';
-
 
 /*
   Routes
 */
 var routes = (
-  <Router history ={createBrowserHistory()}>
+  <Router history ={createHistory()}>
     <Route path='/' component={StorePicker}/>
     <Route path='/store/:storeId' component={App}/>
     <Route path='*' component={NotFound}/>
